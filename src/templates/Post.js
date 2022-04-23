a00d2be1a1a0126baab6f1940c4a84a5907a10d4
casibanryan <restyochea@gmail.com>
2022-04-23 19:39:33 +0800

    updating

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -46,19 +46,6 @@ function Post() {
                             ))}
 
 
-                            {blogData.map((blog, index) => (
-                                <div className="post-preview" key={index}>
-                                    <Link to={`post/${blog.subtitle.split(' ').join('-')}`}>
-                                        <h2 className="post-title" >  {blog.title}  </h2>
-                                        <h3 className="post-subtitle"> {blog.subtitle} </h3>
-                                    </Link>
-                                    <p className="post-meta" >
-                                        Posted by Resty Ochea on {blog.postedOn}
-                                    </p>
-                                    <hr className="my-4" />
-                                </div>
-                            ))}
-
                         </div>
                     </div>
                 </div>
