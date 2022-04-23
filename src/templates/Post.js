38e2272bbc0cc7d45b45dc683f237740793f8dbc
casibanryan <restyochea@gmail.com>
2022-04-23 17:11:16 +0800

    updating

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -34,31 +34,14 @@ function Post() {
             <article className="mb-4">
                 <div className="container px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
-                        <div className="col-md-10 col-lg-8 col-xl-7">
-                            {blog.content.map((paragraph, index) => (
-                                <p key={index}> {paragraph} </p>
-                            ))}
+                        {blog.map((paragraph, index) => (
+                            <div className="col-md-10 col-lg-8 col-xl-7" key={index}>
+                                <p> {paragraph} </p>
+                                <h2 className="section-heading"> {blog.subtitle} </h2>
+                            </div>
+                        ))}
 
-                            <h2 className="section-heading">Reaching for the Stars</h2>
-                            <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the
-                                size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked
-                                so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart.
-                                Seeing this has to change a man.</p>
-                            <a href="#!"><img className="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a>
-                            <span className="caption text-muted">To go places and do things that have never been done before –
-                                that’s what living is all about.</span>
-                            <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year
-                                mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go
-                                where no man has gone before.</p>
-                            <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a
-                                fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
-                            <p>
-                                Placeholder text by
-                                <a href="http://spaceipsum.com/">Space Ipsum</a>
-                                &middot; Images by
-                                <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
-                            </p>
-                        </div>
+
                     </div>
                 </div>
             </article>
