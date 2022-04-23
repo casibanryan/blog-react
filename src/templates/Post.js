132c0f2df424c391a650b431e2c9e8be555865fd
casibanryan <restyochea@gmail.com>
2022-04-23 23:51:48 +0800

    update

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -47,7 +47,7 @@ function Post() {
                                 </React.Fragment>
                             ))}
                             <h1 className="text-center text-success">Related Blog</h1>
-                            <BlogListPage data={relatedBlog} />
+                            <BlogListPage props={relatedBlog} />
                         </div>
                     </div>
                 </div>
