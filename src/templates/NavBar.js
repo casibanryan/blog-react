8a4d77ae3df2825e9a493d486717aca79fdb066c
casibanryan <restyochea@gmail.com>
2022-04-23 19:53:50 +0800

    updating

--- src/templates/NavBar.js
+++ src/templates/NavBar.js
@@ -6,6 +6,28 @@ import { Helmet } from 'react-helmet';
 
 
 function NavBar() {
+    window.addEventListener('DOMContentLoaded', () => {
+        let scrollPos = 0;
+        const mainNav = document.getElementById('mainNav');
+        const headerHeight = mainNav.clientHeight;
+        window.addEventListener('scroll', function () {
+            const currentTop = document.body.getBoundingClientRect().top * -1;
+            if (currentTop < scrollPos) {
+                if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                    mainNav.classList.add('is-visible');
+                } else {
+                    console.log(123);
+                    mainNav.classList.remove('is-visible', 'is-fixed');
+                }
+            } else {
+                mainNav.classList.remove(['is-visible']);
+                if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                    mainNav.classList.add('is-fixed');
+                }
+            }
+            scrollPos = currentTop;
+        });
+    })
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
