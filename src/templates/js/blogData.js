5dfdbcf73710a12323ea1e067a250bb2dfeb7b01
casibanryan <restyochea@gmail.com>
2022-04-23 17:01:16 +0800

    updating

--- src/templates/js/blogData.js
+++ src/templates/js/blogData.js
@@ -1,4 +1,7 @@
-import homeBg from '../assets/img/home-bg.png';
+import underwaterBg from '../assets/img/post-background/underwater-bg.png';
+import fallsBg from '../assets/img/post-background/falls-bg.png'
+import fallsImg from '../assets/img/post-image/falls.jpg';
+import turtleImg from '../assets/img/post-image/turtle.jpg';
 
 
 const date = new Date();
@@ -17,17 +20,26 @@ const blogData = [
   {
     title: 'One of the things that makes Cebu popular is its awesome marine life and diving spots.',
     subtitle: 'Amazing underwater life',
-    content: 'One of the things that makes Cebu popular is its awesome marine life and diving spots. Just a few minutes drive away from the main city will take you to diving spots with Mactan Island where you can witness a school of sardines swimming every now and then. There are also great diving sites along the Mactan Channel if you don’t want to travel far. But if you want some deep diving adventure, drive 2-4 hours north or south. You can find some school of sardines, thresher sharks, whale sharks and sea turtles along Moalboal’s Panagsama Beach and Pescador Island in the south western region. Malapascua Island, a 3-hr drive up north and a boat ride away is famous for its white tip sharks.',
-    background: homeBg,
-    img: 'post-image1',
+    background: fallsBg,
     postedOn: postedDate,
+    content: [
+      'One of the things that makes Cebu popular is its awesome marine life and diving spots. Just a few minutes drive away from the main city will take you to diving spots with Mactan Island where you can witness a school of sardines swimming every now and then. There are also great diving sites along the Mactan Channel if you don’t want to travel far. But if you want some deep diving adventure, drive 2-4 hours north or south. You can find some school of sardines, thresher sharks, whale sharks and sea turtles along Moalboal’s Panagsama Beach and Pescador Island in the south western region. Malapascua Island, a 3-hr drive up north and a boat ride away is famous for its white tip sharks.',
+      turtleImg,
+      'Cebu’s neighbouring islands, Bohol and Negros are both home to amazing underwater life too. You only need to ride a fast craft boat or ferry to take you to those islands. You can book your tickets for Cebu to Bohol here and back.'
+    ],
+
   },
   {
     title: 'Cebu is home to an incredible number of spectacular waterfalls.',
     subtitle: 'Living waterfalls',
-    content: "With a mountainous spine and abundant rainforest, Cebu is home to an incredible number of spectacular waterfalls. Kawasan Falls is arguably the most beguiling, boasting a beautiful setting and a pool of water that is an extraordinary shade of blue. It’s perfect for thrill-seekers too: the area is a popular spot for waterfall jumping, rapelling, rock climbing and canyoning. Tumalog Falls, near Oslob, is worth a visit too. Inside the cascade you'll find lots of convenient natural nooks and crannies, forming perfect seats for a therapeutic water massage.",
-    img: 'post-image2',
+    background: underwaterBg,
     postedOn: postedDate,
+    content: [
+      "Kawasan Falls is one of the top Cebu South tourist spots. It is a three-stage cascade of clear turquoise water from the mountain springs of Mantalongon mountain range. Aside from its natural beauty, Kawasan Falls has also become famous because one of the most popular activities in Cebu is done here—Canyoneering. The canyoneering adventure starts at Canlaob river downstream going to Kawasan Falls.There are local accredited guides who accompany all tourists and facilitate activities at this tourist attraction in Cebu such as walking, jumping, swimming, climbing, and abseiling through the streams.",
+      fallsImg,
+      "With a mountainous spine and abundant rainforest, Cebu is home to an incredible number of spectacular waterfalls. Kawasan Falls is arguably the most beguiling, boasting a beautiful setting and a pool of water that is an extraordinary shade of blue. It’s perfect for thrill-seekers too: the area is a popular spot for waterfall jumping, rapelling, rock climbing and canyoning. Tumalog Falls, near Oslob, is worth a visit too. Inside the cascade you'll find lots of convenient natural nooks and crannies, forming perfect seats for a therapeutic water massage.",
+    ],
+
   }
 
 ];
