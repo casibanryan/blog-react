2493ef5f958fd6336f68ebd963e8bf85153b36d9
casibanryan <restyochea@gmail.com>
2022-04-23 19:34:52 +0800

    updating

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import blogData from './js/blogData';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import BlogList from './component/BlogList';
 
 
 
@@ -30,21 +30,7 @@ function Home() {
             <div className="container px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
                     <div className="col-md-10 col-lg-8 col-xl-7">
-
-                        {blogData.map((blog, index) => (
-                            <div className="post-preview" key={index}>
-                                <Link to={`post/${blog.subtitle.split(' ').join('-')}`}>
-                                    <h2 className="post-title" >  {blog.title}  </h2>
-                                    <h3 className="post-subtitle"> {blog.subtitle} </h3>
-                                </Link>
-                                <p className="post-meta" >
-                                    Posted by Resty Ochea on {blog.postedOn}
-                                </p>
-                                <hr className="my-4" />
-                            </div>
-                        ))}
-
-
+                        <BlogList data={blogData} />
                         <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older
                             Posts →</a></div>
                     </div>
