8c53e01ec5d9019297b57c1596a9f647b5c915f4
casibanryan <restyochea@gmail.com>
2022-04-24 01:43:41 +0800

    update

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -25,15 +25,6 @@ function Home() {
                 </div>
             </header>
 
-            <div className="container px-4 px-lg-5">
-                <div className="row gx-4 gx-lg-5 justify-content-center">
-                    <div className="col-md-10 col-lg-8 col-xl-7">
-                        <BlogListPage />
-                        <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older
-                            Posts →</a></div>
-                    </div>
-                </div>
-            </div>
 
             <Footer />
             <Helmet>
