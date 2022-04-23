84cdff4d869ef19e2391958c72b8d8747ede41bf
casibanryan <restyochea@gmail.com>
2022-04-24 01:45:36 +0800

    update

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -25,6 +25,13 @@ function Home() {
                 </div>
             </header>
 
+            <div className="container px-4 px-lg-5">
+                <div className="row gx-4 gx-lg-5 justify-content-center">
+                    <div className="col-md-10 col-lg-8 col-xl-7">
+                        <BlogListPage />
+                    </div>
+                </div>
+            </div>
 
             <Footer />
             <Helmet>
