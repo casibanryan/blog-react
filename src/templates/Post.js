657e1641f0b983bb352465daf04b4905962b6a46
casibanryan <restyochea@gmail.com>
2022-04-23 17:24:37 +0800

    updating

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -34,13 +34,13 @@ function Post() {
             <article className="mb-4">
                 <div className="container px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
+
                         {blog.content.map((paragraph, index) => (
                             <div className="col-md-10 col-lg-8 col-xl-7" key={index}>
                                 <p> {paragraph} </p>
                                 <h2 className="section-heading"> {blog.subtitle} </h2>
                             </div>
                         ))}
-
 
                     </div>
                 </div>
